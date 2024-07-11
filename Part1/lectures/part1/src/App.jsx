03341f85187251d4164dea22244eb751bcbcae20
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-10 21:27:40 -0400

    finished jsx and multiple components sections

--- Part1/lectures/part1/src/App.jsx
+++ Part1/lectures/part1/src/App.jsx
@@ -1,7 +1,18 @@
-const App = () => {
+const Hello = () => {
   return (
     <div>
       <p>Hello world</p>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello />
+      <Hello />
+      <Hello />
     </div>
   );
 };
