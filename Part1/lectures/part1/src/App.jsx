f6db64bdd5ea1bf98972327aad5b1736ebf9cb99
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-10 21:35:05 -0400

    finished lecture 1a

--- Part1/lectures/part1/src/App.jsx
+++ Part1/lectures/part1/src/App.jsx
@@ -1,18 +1,22 @@
-const Hello = () => {
+const Hello = (props) => {
+  console.log(props);
   return (
     <div>
-      <p>Hello world</p>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
     </div>
   );
 };
 
 const App = () => {
+  const name = 'Peter';
+  const age = 10;
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello />
-      <Hello />
-      <Hello />
+      <Hello name='Maya' age={26 + 10} />
+      <Hello name='Daisy' age={age} />
     </div>
   );
 };
