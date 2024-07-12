0146fba4859d2d9763e22d7355c0b3acb9ad00b0
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-12 06:25:57 -0400

    finished part 2a (~30 mins)

--- Part2/lectures/notes/src/App.jsx
+++ Part2/lectures/notes/src/App.jsx
@@ -1,13 +1,13 @@
-const App = (props) => {
-  const { notes } = props;
+import Note from './components/Note';
 
+const App = ({ notes }) => {
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
       </ul>
     </div>
   );
