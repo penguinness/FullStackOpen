c2e5902ce2c331fd8c29799f4fcc0c8d50070c9a
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-12 02:24:26 -0400

    initialized notes for part 2

---
+++ Part2/lectures/notes/src/App.jsx
@@ -0,0 +1,16 @@
+const App = (props) => {
+  const { notes } = props;
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        <li>{notes[0].content}</li>
+        <li>{notes[1].content}</li>
+        <li>{notes[2].content}</li>
+      </ul>
+    </div>
+  );
+};
+
+export default App;
