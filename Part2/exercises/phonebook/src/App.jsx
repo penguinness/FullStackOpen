022cda070ffb189ca31d9aa04cccab14e0fbeb0e
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-12 11:39:57 -0400

    got the search filter down, still working on it

--- Part2/exercises/phonebook/src/App.jsx
+++ Part2/exercises/phonebook/src/App.jsx
@@ -1,22 +1,78 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
+
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+  const [searchedName, setSearchedName] = useState('');
+
+  console.log(persons);
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    };
+    console.log(personObject);
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+    } else {
+      setPersons(persons.concat(personObject));
+      setNewName('');
+      setNewNumber('');
+    }
+  };
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    setSearchedName(event.target.value);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <div>
+        filter shown with <input value={searchedName} onChange={handleSearch} />
+      </div>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map((person) => {
+          return (
+            <p key={person.name}>
+              {person.name} {person.number}
+            </p>
+          );
+        })}
+      </div>
+      {/* <div>
+        {persons.filter((person) => person.name.includes(searchedName))}
+      </div> */}
     </div>
   );
 };
