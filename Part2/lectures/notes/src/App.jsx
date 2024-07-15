10186fc9cb68564f092d5cb698b18e6263034c52
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-14 20:50:34 -0400

    finished inline styles (i.e. first half of part 2e)

--- Part2/lectures/notes/src/App.jsx
+++ Part2/lectures/notes/src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('a new note...');
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('some error happened...');
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => setNotes(initialNotes));
@@ -27,6 +28,13 @@ const App = () => {
     setNewNote(event.target.value);
   };
 
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return null;
+    }
+    return <div className='error'>{message}</div>;
+  };
+
   const toggleImportanceOf = (id) => {
     const note = notes.find((n) => n.id === id);
     const changedNote = { ...note, important: !note.important };
@@ -37,9 +45,30 @@ const App = () => {
         setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))
       )
       .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from server`);
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
+  };
+
+  const Footer = () => {
+    const footerStyle = {
+      color: 'green',
+      fontStyle: 'italic',
+      fontSize: 16,
+    };
+    return (
+      <div style={footerStyle}>
+        <br />
+        <em>
+          Note app, Department of Computer Science, University of Helsinki 2024
+        </em>
+      </div>
+    );
   };
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
@@ -47,6 +76,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -65,6 +95,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type='submit'>save</button>
       </form>
+      <Footer />
     </div>
   );
 };
