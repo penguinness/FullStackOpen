a9cae198e4f3b03d3d1d54da606b4cd53712bff4
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-15 18:08:45 -0400

    show button is ready but it doesn't render fsr

--- Part2/exercises/countries/src/components/Display.jsx
+++ Part2/exercises/countries/src/components/Display.jsx
@@ -1,14 +1,23 @@
 import ShowInfo from './ShowInfo';
 
 const Display = ({ countries }) => {
+  const handleShowInfo = (country) => {
+    return <ShowInfo country={country} />;
+  };
+
   if (countries.length > 10) {
     return <div>Too many matches, specify another filter</div>;
   } else if (countries.length <= 10 && countries.length > 1) {
     return (
       <div>
-        {countries.map((country) => (
-          <div key={country.name.common}>{country.name.common}</div>
-        ))}
+        {countries.map((country) => {
+          return (
+            <div key={country.name.common}>
+              <div>{country.name.common}</div>{' '}
+              <button onClick={() => handleShowInfo(country)}>show</button>
+            </div>
+          );
+        })}
       </div>
     );
   } else if (countries.length === 1) {
