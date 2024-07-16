92a7e1c9993b886fe171f133059f19d1f41bcd6b
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-16 15:40:45 -0400

    finished part 3a lecture

--- Part3/lectures/index.js
+++ Part3/lectures/index.js
@@ -1,1 +1,90 @@
-console.log('hello world');
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+// const http = require('http');
+
+// 1. Hello World server:
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { 'Content-Type': 'text/plain' });
+//   response.end('Hello World');
+// });
+
+let notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    important: true,
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    important: false,
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    important: true,
+  },
+];
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>');
+});
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes);
+});
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing',
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+// 1. Hello World server (cont)
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { 'Content-Type': 'application/json' });
+//   response.end(JSON.stringify(notes));
+// });
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
