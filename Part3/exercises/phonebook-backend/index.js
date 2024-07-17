e4adf6ae55e9300c1a8944df470cd43558085027
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-17 00:57:18 -0400

    finished exercise 3.9

--- Part3/exercises/phonebook-backend/index.js
+++ Part3/exercises/phonebook-backend/index.js
@@ -112,6 +112,6 @@ app.post('/api/persons', (request, response) => {
   response.json(person);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT);
 console.log(`Server running on port ${PORT}`);
