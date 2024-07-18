b2b27b30f2444920fe1775b408f15708212a8090
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-18 19:11:25 -0400

    finished 1/2 of 3d lecture

--- Part3/lectures/notes-backend/index.js
+++ Part3/lectures/notes-backend/index.js
@@ -81,7 +81,7 @@ const generateId = () => {
   return String(maxId + 1);
 };
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body;
 
   if (body.content === undefined) {
@@ -93,20 +93,22 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
   });
 
-  note.save().then((savedNote) => {
-    response.json(savedNote);
-  });
+  note
+    .save()
+    .then((savedNote) => {
+      response.json(savedNote);
+    })
+    .catch((error) => next(error));
 });
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body;
+  const { content, important } = request.body;
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then((updatedNote) => {
       response.json(updatedNote);
     })
@@ -125,6 +127,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
--- Part3/lectures/notes-backend/models/note.js
+++ Part3/lectures/notes-backend/models/note.js
@@ -17,7 +17,11 @@ mongoose
   });
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
   important: Boolean,
 });
 
