ab33195616626b6f6c52c9bee0a0797b56c50809
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-18 17:59:36 -0400

    finished part 3c exercises

--- Part3/exercises/phonebook-backend/index.js
+++ Part3/exercises/phonebook-backend/index.js
@@ -61,16 +61,18 @@ app.get('/api/persons', (request, response) => {
 });
 
 app.get('/api/info', (request, response) => {
-  const info = `<p>Phonebook has info for ${
-    persons.length
-  } people</p><p>${new Date().toString()}</p>`;
-  response.send(info);
+  Person.countDocuments({}).then((count) => {
+    const info = `<p>Phonebook has info for ${count} people</p><p>${new Date().toString()}</p>`;
+    response.send(info);
+  });
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then((person) => {
-    response.json(person);
-  });
+  Person.findById(request.params.id)
+    .then((person) => {
+      response.json(person);
+    })
+    .catch((error) => next(error));
 });
 
 app.delete('/api/persons/:id', (request, response, next) => {
@@ -121,6 +123,35 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson);
   });
 });
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    randomID: body.randomID,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT);
