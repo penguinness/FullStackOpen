acbeacbcf3258a835ce84506796cddf73cc1fda0
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-18 16:46:03 -0400

    modified delete to use db

--- Part3/exercises/phonebook-backend/index.js
+++ Part3/exercises/phonebook-backend/index.js
@@ -73,11 +73,12 @@ app.get('/api/persons/:id', (request, response) => {
   });
 });
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const generateId = () => {
