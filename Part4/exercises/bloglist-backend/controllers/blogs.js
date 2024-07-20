0b3b747ddec84e0334898997ec6cab9e4481a117
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-19 23:16:30 -0400

    finished 4.1-4.2

---
+++ Part4/exercises/bloglist-backend/controllers/blogs.js
@@ -0,0 +1,17 @@
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+blogsRouter.post('/', (request, response, next) => {
+  const blog = new Blog(request.body);
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+module.exports = blogsRouter;
