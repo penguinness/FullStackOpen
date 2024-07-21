847fbff1a240d3def48010c1bfb360e2a177d5fb
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-20 22:42:38 -0400

    finished 4.3

---
+++ Part4/exercises/bloglist-backend/tests/list_helper.test.js
@@ -0,0 +1,10 @@
+const { test, describe } = require('node:test');
+const assert = require('node:assert');
+const listHelper = require('../utils/list_helper');
+
+test('dummy returns one', () => {
+  const blogs = [];
+
+  const result = listHelper.dummy(blogs);
+  assert.strictEqual(result, 1);
+});
