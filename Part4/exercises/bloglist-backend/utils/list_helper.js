1e3659f9b17300d8916df95f860c236c5e52db9e
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-20 23:12:11 -0400

    finished 4.5

--- Part4/exercises/bloglist-backend/utils/list_helper.js
+++ Part4/exercises/bloglist-backend/utils/list_helper.js
@@ -6,7 +6,21 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const mostLikedBlog = blogs.reduce((mostLikedBlog, currentBlog) => {
+    return currentBlog.likes > mostLikedBlog.likes
+      ? currentBlog
+      : mostLikedBlog;
+  }, blogs[0]);
+  return {
+    title: mostLikedBlog.title,
+    author: mostLikedBlog.author,
+    likes: mostLikedBlog.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
