298319731d8ec958bfedc0040676f45be5d55f7d
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-23 18:19:02 -0400

    finished 4b

--- Part4/exercises/bloglist-backend/controllers/blogs.js
+++ Part4/exercises/bloglist-backend/controllers/blogs.js
@@ -33,4 +33,30 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog);
 });
 
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id);
+  response.status(204).end();
+});
+
+blogsRouter.put('/:id', async (request, response) => {
+  const { title, author, url, likes } = request.body;
+
+  if (!title || !url) {
+    return response.status(400).json({ error: 'title or url missing' });
+  }
+
+  const blog = {
+    title,
+    author,
+    url,
+    likes: likes || 0,
+  };
+
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
+    new: true,
+  });
+
+  response.json(updatedBlog);
+});
+
 module.exports = blogsRouter;
--- Part4/exercises/bloglist-backend/tests/blog_api.test.js
+++ Part4/exercises/bloglist-backend/tests/blog_api.test.js
@@ -105,6 +105,20 @@ test('blog without url is not added', async () => {
   assert.strictEqual(response.body.length, helper.initialBlogs.length);
 });
 
+test('a blog with valid id can be deleted', async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToDelete = blogsAtStart[0];
+
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+  const blogsAtEnd = await helper.blogsInDb();
+
+  assert.strictEqual(blogsAtEnd.length, helper.initialBlogs.length - 1);
+
+  const titles = blogsAtEnd.map((r) => r.title);
+  assert(!titles.includes(blogToDelete.title));
+});
+
 after(async () => {
   await mongoose.connection.close();
 });
