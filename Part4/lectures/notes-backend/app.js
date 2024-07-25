3f7ecc649d1fe4e7ba75b4554403740de6540077
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-24 20:25:51 -0400

    finished 4d lecture

--- Part4/lectures/notes-backend/app.js
+++ Part4/lectures/notes-backend/app.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require('cors');
 const notesRouter = require('./controllers/notes');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
@@ -29,6 +30,7 @@ app.use(middleware.requestLogger);
 
 app.use('/api/notes', notesRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
--- Part4/lectures/notes-backend/utils/middleware.js
+++ Part4/lectures/notes-backend/utils/middleware.js
@@ -24,6 +24,12 @@ const errorHandler = (error, request, response, next) => {
     return response
       .status(400)
       .json({ error: 'expected `username` to be unique' });
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'token invalid' });
+  } else if (error.name === 'TokenExpiredError') {
+    return response.status(401).json({
+      error: 'token expired',
+    });
   }
 
   next(error);
