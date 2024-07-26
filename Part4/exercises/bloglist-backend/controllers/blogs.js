8b27e9d198c3834778306f4b1a0b161e959d02bb
penguinness <92165891+penguinness@users.noreply.github.com>
2024-07-25 20:39:46 -0400

    finished 4.20

--- Part4/exercises/bloglist-backend/controllers/blogs.js
+++ Part4/exercises/bloglist-backend/controllers/blogs.js
@@ -28,7 +28,7 @@ blogsRouter.get('/:id', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' });
   }
--- Part4/exercises/bloglist-backend/utils/middleware.js
+++ Part4/exercises/bloglist-backend/utils/middleware.js
@@ -8,6 +8,18 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
+const tokenExtractor = (request, response, next) => {
+  // code that extracts the token
+  const authorization = request.get('authorization');
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.substring(7);
+  } else {
+    request.token == null;
+  }
+
+  next();
+};
+
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' });
 };
@@ -39,6 +51,7 @@ const errorHandler = (error, request, response, next) => {
 
 module.exports = {
   requestLogger,
+  tokenExtractor,
   unknownEndpoint,
   errorHandler,
 };
