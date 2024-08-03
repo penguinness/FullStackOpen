5fead9c97f24d095edd81fa0da8420343a9e17be
penguinness <92165891+penguinness@users.noreply.github.com>
2024-08-02 22:17:34 -0400

    added prop-types to notes (this is funny)

--- Part5/lectures/notes/src/components/Togglable.jsx
+++ Part5/lectures/notes/src/components/Togglable.jsx
@@ -1,20 +1,22 @@
-import { useState, useImperativeHandle, forwardRef } from 'react'
+import { useState, useImperativeHandle, forwardRef } from 'react';
+
+import PropTypes from 'prop-types';
 
 const Togglable = forwardRef((props, ref) => {
-  const [visible, setVisible] = useState(false)
+  const [visible, setVisible] = useState(false);
 
-  const hideWhenVisible = { display: visible ? 'none' : '' }
-  const showWhenVisible = { display: visible ? '' : 'none' }
+  const hideWhenVisible = { display: visible ? 'none' : '' };
+  const showWhenVisible = { display: visible ? '' : 'none' };
 
   const toggleVisibility = () => {
-    setVisible(!visible)
-  }
+    setVisible(!visible);
+  };
 
   useImperativeHandle(ref, () => {
     return {
       toggleVisibility,
-    }
-  })
+    };
+  });
 
   return (
     <div>
@@ -26,7 +28,13 @@ const Togglable = forwardRef((props, ref) => {
         <button onClick={toggleVisibility}>cancel</button>
       </div>
     </div>
-  )
-})
+  );
+});
 
-export default Togglable
+Togglable.displayName = 'Togglable';
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+};
+
+export default Togglable;
