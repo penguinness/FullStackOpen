f2306e7c96b1f2c453e94b9f3b6944f07155cb41
penguinness <92165891+penguinness@users.noreply.github.com>
2024-08-06 17:27:27 -0400

    finished 6a

--- Part6/lectures/redux-notes/src/main.jsx
+++ Part6/lectures/redux-notes/src/main.jsx
@@ -1,5 +1,16 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import { legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+import App from './App';
+import noteReducer from './reducers/noteReducer';
+
+const store = createStore(noteReducer);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
--- Part6/lectures/redux-notes/src/reducers/noteReducer.js
+++ Part6/lectures/redux-notes/src/reducers/noteReducer.js
@@ -2,7 +2,7 @@ const noteReducer = (state = [], action) => {
   switch (action.type) {
     case 'NEW_NOTE':
       return [...state, action.payload];
-    case 'TOGGLE_IMPORTANCE': {
+    case 'TOGGLE_IMPORTANCE':
       const id = action.payload.id;
       const noteToChange = state.find((n) => n.id === id);
       const changedNote = {
@@ -10,10 +10,29 @@ const noteReducer = (state = [], action) => {
         important: !noteToChange.important,
       };
       return state.map((note) => (note.id !== id ? note : changedNote));
-    }
     default:
       return state;
   }
+};
+
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+
+export const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    payload: {
+      content,
+      important: false,
+      id: generateId(),
+    },
+  };
+};
+
+export const toggleImportanceOf = (id) => {
+  return {
+    type: 'TOGGLE_IMPORTANCE',
+    payload: { id },
+  };
 };
 
 export default noteReducer;
