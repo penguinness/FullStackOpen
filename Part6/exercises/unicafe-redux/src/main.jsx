24c6ca988104480809aafc53b9d489454939a487
penguinness <92165891+penguinness@users.noreply.github.com>
2024-08-06 00:26:30 -0400

    finished 6.1 - 6.2

--- Part6/exercises/unicafe-redux/src/main.jsx
+++ Part6/exercises/unicafe-redux/src/main.jsx
@@ -1,36 +1,54 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import { createStore } from 'redux'
-import reducer from './reducer'
+import { legacy_createStore as createStore } from 'redux';
+import reducer from './reducer';
 
-const store = createStore(reducer)
+const store = createStore(reducer);
 
 const App = () => {
   const good = () => {
     store.dispatch({
-      type: 'GOOD'
-    })
-  }
+      type: 'GOOD',
+    });
+  };
+
+  const ok = () => {
+    store.dispatch({
+      type: 'OK',
+    });
+  };
+
+  const bad = () => {
+    store.dispatch({
+      type: 'BAD',
+    });
+  };
+
+  const reset = () => {
+    store.dispatch({
+      type: 'RESET',
+    });
+  };
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={good}>good</button>
+      <button onClick={ok}>ok</button>
+      <button onClick={bad}>bad</button>
+      <button onClick={reset}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
-  )
-}
+  );
+};
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const renderApp = () => {
-  root.render(<App />)
-}
+  root.render(<App />);
+};
 
-renderApp()
-store.subscribe(renderApp)
+renderApp();
+store.subscribe(renderApp);
--- Part6/exercises/unicafe-redux/src/reducer.js
+++ Part6/exercises/unicafe-redux/src/reducer.js
@@ -1,23 +1,23 @@
 const initialState = {
   good: 0,
   ok: 0,
-  bad: 0
-}
+  bad: 0,
+};
 
 const counterReducer = (state = initialState, action) => {
-  console.log(action)
+  console.log(action);
   switch (action.type) {
     case 'GOOD':
-      return state
+      return { ...state, good: state.good + 1 };
     case 'OK':
-      return state
+      return { ...state, ok: state.ok + 1 };
     case 'BAD':
-      return state
-    case 'ZERO':
-      return state
-    default: return state
+      return { ...state, bad: state.bad + 1 };
+    case 'RESET':
+      return initialState;
+    default:
+      return state;
   }
-  
-}
+};
 
-export default counterReducer
+export default counterReducer;
