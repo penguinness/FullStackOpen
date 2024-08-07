0495fa684d677d5e0a2b6695469eb5b5a690d528
penguinness <92165891+penguinness@users.noreply.github.com>
2024-08-07 01:14:37 -0400

    finished 6b up to 6.9

--- Part6/lectures/redux-notes/src/main.jsx
+++ Part6/lectures/redux-notes/src/main.jsx
@@ -1,16 +1,34 @@
+import { createNote } from './reducers/noteReducer';
+import { filterChange } from './reducers/filterReducer';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { legacy_createStore as createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-
 import App from './App';
+
 import noteReducer from './reducers/noteReducer';
 
-const store = createStore(noteReducer);
+import filterReducer from './reducers/filterReducer';
+
+const reducer = combineReducers({
+  notes: noteReducer,
+  filter: filterReducer,
+});
+
+const store = createStore(reducer);
+
+console.log(store.getState());
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <App />
   </Provider>
+);
+
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(filterChange('IMPORTANT'));
+store.dispatch(
+  createNote('combineReducers forms one reducer from many simple reducers')
 );
---
+++ Part6/lectures/redux-notes/src/reducers/filterReducer.js
@@ -0,0 +1,18 @@
+const filterReducer = (state = 'ALL', action) => {
+  console.log('ACTION: ', action);
+  switch (action.type) {
+    case 'SET_FILTER':
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export const filterChange = (filter) => {
+  return {
+    type: 'SET_FILTER',
+    payload: filter,
+  };
+};
+
+export default filterReducer;
