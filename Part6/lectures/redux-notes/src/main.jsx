7f254909bb2132c7e3f54e1342c2fa64763cfbef
penguinness <92165891+penguinness@users.noreply.github.com>
2024-08-07 21:14:05 -0400

    finished 6c lecture up to exercises 6.14-6.15

--- Part6/lectures/redux-notes/src/main.jsx
+++ Part6/lectures/redux-notes/src/main.jsx
@@ -4,7 +4,9 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import App from './App';
 
-import noteReducer from './reducers/noteReducer';
+import noteService from './services/notes';
+
+import noteReducer, { setNotes } from './reducers/noteReducer';
 import filterReducer from './reducers/filterReducer';
 
 const store = configureStore({
@@ -13,6 +15,8 @@ const store = configureStore({
     filter: filterReducer,
   },
 });
+
+noteService.getAll().then((notes) => store.dispatch(setNotes(notes)));
 
 console.log(store.getState());
 
--- Part6/lectures/redux-notes/src/reducers/noteReducer.js
+++ Part6/lectures/redux-notes/src/reducers/noteReducer.js
@@ -17,15 +17,11 @@ const generateId = () => Number((Math.random() * 1000000).toFixed(0));
 
 const noteSlice = createSlice({
   name: 'notes',
-  initialState,
+  initialState: [],
   reducers: {
     createNote(state, action) {
-      const content = action.payload;
-      state.push({
-        content,
-        important: false,
-        id: generateId(),
-      });
+      const newNote = action.payload;
+      state.push(newNote);
     },
     toggleImportanceOf(state, action) {
       const id = action.payload;
@@ -39,8 +35,15 @@ const noteSlice = createSlice({
 
       return state.map((note) => (note.id !== id ? note : changedNote));
     },
+    appendNote(state, action) {
+      state.push(action.payload);
+    },
+    setNotes(state, action) {
+      return action.payload;
+    },
   },
 });
 
-export const { createNote, toggleImportanceOf } = noteSlice.actions;
+export const { createNote, toggleImportanceOf, appendNote, setNotes } =
+  noteSlice.actions;
 export default noteSlice.reducer;
