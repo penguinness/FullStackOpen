9f0067f64b476b77971ac32d25e4a89ef380fcee
penguinness <92165891+penguinness@users.noreply.github.com>
2024-08-08 16:50:07 -0400

    finished 6.18

--- Part6/exercises/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ Part6/exercises/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -27,28 +27,24 @@ const anecdoteSlice = createSlice({
   name: 'anecdotes',
   initialState: [],
   reducers: {
-    addVote(state, action) {
-      const id = action.payload;
-      const anecdoteToChange = state.find((a) => a.id === id);
-      console.log(current(anecdoteToChange));
-      const changedAnecdote = {
-        ...anecdoteToChange,
-        votes: anecdoteToChange.votes + 1,
-      };
-      return state.map((anecdote) =>
-        anecdote.id !== id ? anecdote : changedAnecdote
-      );
-    },
     appendAnecdote(state, action) {
       state.push(action.payload);
     },
     setAnecdotes(state, action) {
       return action.payload;
     },
+    updateAnecdote(state, action) {
+      const id = action.payload.id;
+      const changedAnecdote = action.payload;
+      return state.map((anecdote) =>
+        anecdote.id !== id ? anecdote : changedAnecdote
+      );
+    },
   },
 });
 
-export const { addVote, appendAnecdote, setAnecdotes } = anecdoteSlice.actions;
+export const { updateAnecdote, appendAnecdote, setAnecdotes } =
+  anecdoteSlice.actions;
 
 export const initializeAnecdotes = () => {
   return async (dispatch) => {
@@ -61,6 +57,19 @@ export const createAnecdote = (content) => {
   return async (dispatch) => {
     const newAnecdote = await anecdoteService.createNew(content);
     dispatch(appendAnecdote(newAnecdote));
+  };
+};
+
+export const addVote = (id) => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    const anecdoteToChange = anecdotes.find((a) => a.id === id);
+    const changedAnecdote = {
+      ...anecdoteToChange,
+      votes: anecdoteToChange.votes + 1,
+    };
+    const response = await anecdoteService.update(id, changedAnecdote);
+    dispatch(updateAnecdote(response));
   };
 };
 
--- Part6/exercises/redux-anecdotes/src/services/anecdotes.js
+++ Part6/exercises/redux-anecdotes/src/services/anecdotes.js
@@ -13,4 +13,9 @@ const createNew = async (content) => {
   return response.data;
 };
 
-export default { getAll, createNew };
+const update = async (id, newContent) => {
+  const response = await axios.put(`${baseUrl}/${id}`, newContent);
+  return response.data;
+};
+
+export default { getAll, createNew, update };
