b6e653da8dc93d06ed3874ca4faf4a5be41818c6
penguinness <92165891+penguinness@users.noreply.github.com>
2024-08-09 16:54:52 -0400

    finished 7.1

--- Part7/exercises/routed-anecdotes/src/main.jsx
+++ Part7/exercises/routed-anecdotes/src/main.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import { BrowserRouter as Router } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <App />
+  </Router>
+);
