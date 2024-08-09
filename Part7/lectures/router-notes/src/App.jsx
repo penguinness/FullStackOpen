8749a20a70bcfda11f824e022409b730807646b0
penguinness <92165891+penguinness@users.noreply.github.com>
2024-08-09 15:50:11 -0400

    finished 7a lecture

--- Part7/lectures/router-notes/src/App.jsx
+++ Part7/lectures/router-notes/src/App.jsx
@@ -1,33 +1,133 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import {
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useParams,
+  useNavigate,
+  useMatch,
+} from 'react-router-dom';
 
 const Home = () => (
   <div>
-    {' '}
-    <h2>TKTL notes app</h2>{' '}
+    <h2>TKTL notes app</h2>
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset
+      sheets containing Lorem Ipsum passages, and more recently with desktop
+      publishing software like Aldus PageMaker including versions of Lorem
+      Ipsum.
+    </p>
   </div>
 );
 
-const Notes = () => (
+const Note = ({ note }) => {
+  return (
+    <div>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div>
+        <strong>{note.important ? 'important' : ''}</strong>
+      </div>
+    </div>
+  );
+};
+
+const Notes = ({ notes }) => (
   <div>
-    {' '}
-    <h2>Notes</h2>{' '}
+    <h2>Notes</h2>
+    <ul>
+      {notes.map((note) => (
+        <li key={note.id}>
+          <Link to={`/notes/${note.id}`}>{note.content}</Link>
+        </li>
+      ))}
+    </ul>
   </div>
 );
 
 const Users = () => (
   <div>
-    {' '}
-    <h2>Users</h2>{' '}
+    <h2>TKTL notes app</h2>
+    <ul>
+      <li>Matti Luukkainen</li>
+      <li>Juha Tauriainen</li>
+      <li>Arto Hellas</li>
+    </ul>
   </div>
 );
 
+const Login = (props) => {
+  const navigate = useNavigate();
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    props.onLogin('mluukkai');
+    navigate('/');
+  };
+
+  return (
+    <div>
+      <h2>login</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          username: <input />
+        </div>
+        <div>
+          password: <input type='password' />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  );
+};
+
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      content: 'HTML is easy',
+      important: true,
+      user: 'Matti Luukkainen',
+    },
+    {
+      id: 2,
+      content: 'Browser can execute only JavaScript',
+      important: false,
+      user: 'Matti Luukkainen',
+    },
+    {
+      id: 3,
+      content: 'Most important methods of HTTP-protocol are GET and POST',
+      important: true,
+      user: 'Arto Hellas',
+    },
+  ]);
+
+  const [user, setUser] = useState(null);
+
+  const match = useMatch('/notes/:id');
+
+  const note = match
+    ? notes.find((note) => note.id === Number(match.params.id))
+    : null;
+
+  const login = (user) => {
+    setUser(user);
+  };
+
   const padding = {
     padding: 5,
   };
 
   return (
-    <Router>
+    <div>
       <div>
         <Link style={padding} to='/'>
           home
@@ -38,18 +138,29 @@ const App = () => {
         <Link style={padding} to='/users'>
           users
         </Link>
+        {user ? (
+          <em>{user} logged in</em>
+        ) : (
+          <Link style={padding} to='/login'>
+            login
+          </Link>
+        )}
       </div>
-
       <Routes>
-        <Route path='/notes' element={<Notes />} />
-        <Route path='/users' element={<Users />} />
+        <Route path='/notes/:id' element={<Note note={note} />} />
+        <Route path='/notes' element={<Notes notes={notes} />} />
+        <Route
+          path='/users'
+          element={user ? <Users /> : <Navigate replace to='/login' />}
+        />
+        <Route path='/login' element={<Login onLogin={login} />} />
         <Route path='/' element={<Home />} />
       </Routes>
-
-      <div>
-        <i>Note app, Department of Computer Science 2024</i>
-      </div>
-    </Router>
+      <footer>
+        <br />
+        <em>Note app, Department of Computer Science 2024</em>
+      </footer>
+    </div>
   );
 };
 
--- Part7/lectures/router-notes/src/main.jsx
+++ Part7/lectures/router-notes/src/main.jsx
@@ -1,5 +1,11 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Router>
+    <App />
+  </Router>
+);
