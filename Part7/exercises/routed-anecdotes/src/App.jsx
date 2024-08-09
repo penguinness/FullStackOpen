bc69c070a4fce89c00ecc8ba333a2b7ff3b994c1
penguinness <92165891+penguinness@users.noreply.github.com>
2024-08-09 17:10:06 -0400

    finished 7.2

--- Part7/exercises/routed-anecdotes/src/App.jsx
+++ Part7/exercises/routed-anecdotes/src/App.jsx
@@ -31,12 +31,26 @@ const Menu = () => {
   );
 };
 
+const Anecdote = ({ anecdote }) => {
+  return (
+    <div>
+      <h2>{anecdote.content}</h2>
+      <p>has {anecdote.votes} votes</p>
+      <p>
+        for more info see <a href={anecdote.info}>{anecdote.info}</a>
+      </p>
+    </div>
+  );
+};
+
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
       {anecdotes.map((anecdote) => (
-        <li key={anecdote.id}>{anecdote.content}</li>
+        <li key={anecdote.id}>
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
       ))}
     </ul>
   </div>
@@ -142,6 +156,12 @@ const App = () => {
     },
   ]);
 
+  const match = useMatch('/anecdotes/:id');
+
+  const anecdote = match
+    ? anecdotes.find((anecdote) => anecdote.id === Number(match.params.id))
+    : null;
+
   const [notification, setNotification] = useState('');
 
   const addNew = (anecdote) => {
@@ -167,6 +187,10 @@ const App = () => {
       <h1>Software anecdotes</h1>
       <Menu />
       <Routes>
+        <Route
+          path='/anecdotes/:id'
+          element={<Anecdote anecdote={anecdote} />}
+        />
         <Route path='/' element={<AnecdoteList anecdotes={anecdotes} />} />
         <Route
           path='/anecdotes'
