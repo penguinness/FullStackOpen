44a14b8246139ead16c266c760e2c4251ec0f9b7
penguinness <92165891+penguinness@users.noreply.github.com>
2024-08-16 23:45:48 -0400

    removed query-bloglist (finishing part 7f with redux-bloglist)

--- Part7/exercises/query-bloglist/bloglist-frontend/src/components/Notification.jsx
+++
@@ -1,8 +0,0 @@
-const Notification = ({ message, className }) => {
-  if (message === null) {
-    return null
-  }
-  return <div className={className}>{message}</div>
-}
-
-export default Notification
